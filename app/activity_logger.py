af201a469dedcee03ec2b90c47fb4c5dc18aba11
Puja <itispujakumari21@gmail.com>
2025-05-31 09:45:37 +0530

    logs data to CSV

---
+++ app/activity_logger.py
@@ -0,0 +1,24 @@
+#  logs data to CSV
+import csv
+from datetime import datetime
+
+def log_usage(app_name, window_title, active_time):
+    with open("usage_log.csv", "a", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow([
+            datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            app_name,
+            window_title,
+            active_time
+        ])
+        
+       
+
+
+
+# testing
+# log_usage("chrome", "youtube", 60)
+# # Print the contents of the CSV file
+# with open("usage_log.csv", "r", encoding="utf-8") as f:
+#     print(f.read())
+# --------
