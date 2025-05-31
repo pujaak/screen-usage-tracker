d066c13ae2ca9a1b66c653d77320ce318f280484
Puja <itispujakumari21@gmail.com>
2025-05-31 09:23:53 +0530

    config constants added

---
+++ app/config.py
@@ -0,0 +1,4 @@
+#  config constants
+IDLE_TIME_LIMIT = 60 #  seconds before considered idle
+TRACK_INTERVAL = 1  # how often to check (in seconds)
+LOG_INTERVAL = 60   # log once every N active seconds
